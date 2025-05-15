7b4dad601ca723d1894202ce9f95d6f040fb8288
kosei-player <ktoeuzskeai@outlook.jp>
2025-05-15 15:48:16 +0900

    Create Re_LEDtest.ino
    RE

---
+++ Re_LEDtest.ino
@@ -0,0 +1,24 @@
+#include<FastLED.h>
+
+#define NUM 50
+
+CRGB leds[NUM];
+
+
+void setup() {
+   FastLED.addLeds<WS2812,6>(leds,NUM);
+  // put your setup code here, to run once:
+
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+ for(int BB=0; BB<=20; BB++)
+ {
+  leds[BB].r=BB*5;
+  leds[BB].g=BB*10;
+  leds[BB].b=BB*20;
+ }
+    
+FastLED.show();
+delay(10);
+}
